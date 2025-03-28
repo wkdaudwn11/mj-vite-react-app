5089643b7ec496894a43922498c728257a7f0ca9
emjayjang <emjay.jang@quantumx.co.kr>
2025-03-28 15:28:58 +0900

    refactor: split count store logic

--- src/shared/store/useCountStore.js
+++ src/shared/store/useCountStore.js
@@ -4,27 +4,32 @@ import { devtools } from 'zustand/middleware';
 const initialState = {
   count: 0,
 };
+
+const createCountSlice = set => ({
+  ...initialState,
+  setIncrease: () =>
+    set(
+      state => ({
+        count: state.count + 1,
+      }),
+      false,
+      'increase'
+    ),
+  setDecrease: () =>
+    set(
+      state => ({
+        count: state.count - 1,
+      }),
+      false,
+      'decrease'
+    ),
+  reset: () => set(initialState, false, 'reset'),
+});
+
 const useCountStore = create(
-  devtools(set => ({
-    ...initialState,
-    setIncrease: () =>
-      set(
-        state => ({
-          count: state.count + 1,
-        }),
-        false,
-        'increase'
-      ),
-    setDecrease: () =>
-      set(
-        state => ({
-          count: state.count - 1,
-        }),
-        false,
-        'decrease'
-      ),
-    reset: () => set(initialState, false, 'reset'),
-  }))
+  import.meta.env.VITE_APP_ENV === 'development'
+    ? devtools(createCountSlice, { name: 'useCountStore' })
+    : createCountSlice
 );
 
 export default useCountStore;
