0e09384a04dbb1eac53d109e18efef6c25b140bb
MJ <wkdaudwn11@naver.com>
2025-03-31 22:42:03 +0900

    feat: add design tokens and TailwindCSS theme setup

--- src/pages/defectChart/DefectChartPage.jsx
+++ src/pages/defectChart/DefectChartPage.jsx
@@ -1,24 +1,38 @@
 import { useCountStore } from '@/shared/store';
+import { theme } from '@/shared/styles';
 
 const DefectChartPage = () => {
   const { count, setIncrease, setDecrease, reset } = useCountStore();
 
   return (
-    <div className='bg-gray-400'>
+    <div className='flex flex-col gap-2 px-4'>
       <h1>DefectChartPage</h1>
-      <h1 className='text-3xl font-bold text-blue-500 underline'>
-        Hello world!
-      </h1>
-      <p>{count}</p>
-      <button type='button' onClick={setIncrease}>
-        +
-      </button>
-      <button type='button' onClick={setDecrease}>
-        -
-      </button>
-      <button type='button' onClick={reset}>
-        reset
-      </button>
+      <p className='text-coral'>coral: {theme.colors.coral}</p>
+      <p className='text-lagoon'>title: {theme.fontSize.title}</p>
+      <p className='text-3xl font-bold text-blue-500 underline'>{count}</p>
+      <div className='flex gap-4'>
+        <button
+          type='button'
+          className='cursor-pointer bg-gray-300 px-2 py-1'
+          onClick={setIncrease}
+        >
+          +
+        </button>
+        <button
+          type='button'
+          className='cursor-pointer bg-gray-400 px-2 py-1'
+          onClick={setDecrease}
+        >
+          -
+        </button>
+        <button
+          type='button'
+          className='cursor-pointer bg-gray-500 px-2 py-1 text-gray-50'
+          onClick={reset}
+        >
+          reset
+        </button>
+      </div>
     </div>
   );
 };
